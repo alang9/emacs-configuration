cd8eab55dfbff5d15c522e3c72c4bd239f13769e
Alex Lang <me@alang.ca>
2012-08-07 19:52:38 +0900

    revert-all-buffers

--- emacs.d/local/interaction.el
+++ emacs.d/local/interaction.el
@@ -19,6 +19,20 @@
   (put 'narrow-to-region 'disabled nil)
 )
 
+;; Revert all buffers
+(defun revert-all-buffers ()
+  "Refreshes all open buffers from their respective files"
+  (interactive)
+  (let* ((list (buffer-list))
+         (buffer (car list)))
+    (while buffer
+      (when (and (buffer-file-name buffer)
+                 (not (buffer-modified-p buffer)))
+        (set-buffer buffer)
+        (revert-buffer t t t))
+      (setq list (cdr list))
+      (setq buffer (car list))))
+  (message "Refreshed open files"))
 
 ;; Keybindings
 
