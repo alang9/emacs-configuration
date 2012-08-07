7456985eef5b80b5f47f8aaa43747783ed9f01e0
Alex Lang <me@alang.ca>
2012-08-07 20:37:56 +0900

    Enable ghc-mod in haskell-mode

--- emacs.d/local/haskell-mode-load.el
+++ emacs.d/local/haskell-mode-load.el
@@ -21,9 +21,9 @@
 ;; Haskell main editing mode key bindings.
 (defun haskell-hook ()
   ;; ghc-mod
-  ;; (autoload 'ghc-init "ghc" nil t)
-  ;; (ghc-init)
-  ;; (flymake-mode)
+  (autoload 'ghc-init "ghc" nil t)
+  (ghc-init)
+  (flymake-mode)
 
   ;; Use simple indentation.
   (turn-on-haskell-simple-indent)
