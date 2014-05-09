32fbc4f8032f05fea52b2dc25e3cd7a19512e72b
Alex Lang <me@alang.ca>
2014-05-08 19:24:46 -0700

    Add some haskell packages in install-packages.sh

--- install-packages.sh
+++ install-packages.sh
@@ -2,4 +2,7 @@
 
 apt-get install emacs23 emacs23-el emacs-goodies-el python-mode python-ropemacs erlang-mode js2-mode xmlstarlet org-mode
 cabal update
-cabal install ghc-mod hlint
+cabal install cabal-install
+cabal update
+cabal install happy alex
+cabal install ghci-ng ghc-mod structured-haskell-mode stylish-haskell
